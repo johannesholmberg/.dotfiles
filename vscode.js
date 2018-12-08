f20a0a7824063395c8fff352fc6d48ed6e01c108
Johannes Holmberg <johannes@holmberg.io>
2018-12-08 08:41:36 +0100

    Sort the code settings

--- vscode.js
+++ vscode.js
@@ -1,49 +1,58 @@
 {
-  "workbench.colorTheme": "Cobalt2",
-  "workbench.editor.tabSizing": "shrink",
-  "editor.fontFamily": "Inconsolata, Monaco, monospace",
-  "editor.fontSize": 17,
+  "editor.fontFamily": "Dank Mono, Fira Code, Inconsolata, Monaco, monospace",
+  "editor.fontSize": 18,
   "editor.tabSize": 2,
   "editor.lineHeight": 25,
-  "editor.letterSpacing": 0.5,
-  "files.trimTrailingWhitespace": true,
+  "editor.letterSpacing": 0.3,
   "editor.renderWhitespace": "all",
-  "workbench.iconTheme": "material-icon-theme",
-  "workbench.sideBar.location": "left",
+  "editor.fontLigatures": true,
   "editor.multiCursorModifier": "ctrlCmd",
   "editor.snippetSuggestions": "top",
   "editor.formatOnPaste": false,
-  "explorer.confirmDelete": false,
-  // use emmet for additional syntax
-  "emmet.triggerExpansionOnTab": true,
-  "emmet.includeLanguages": {
-    "javascript": "javascriptreact",
-    "mustache": "html"
-  },
-  "window.zoomLevel": 1,
-  "explorer.confirmDragAndDrop": false,
+  "editor.formatOnSave": true,
+
+  "breadcrumbs.enabled": true,
+
   "git.confirmSync": false,
   "git.autofetch": true,
-  "workbench.activityBar.visible": true,
-  // this lets you quickly open multiple items in a row and not have the previous ones closed on you
-  "workbench.editor.enablePreviewFromQuickOpen": false,
-  // auto-save configs
-  "editor.formatOnSave": true,
-  "files.autoSave": "onFocusChange",
+
+  "files.trimTrailingWhitespace": true,
+
+  "explorer.confirmDelete": false,
+  "explorer.confirmDragAndDrop": false,
+
+  "eslint.packageManager": "yarn",
   "eslint.autoFixOnSave": true,
   "eslint.alwaysShowStatus": true,
   "[javascript]": {
     "editor.formatOnSave": false
   },
-  "prettier.disableLanguages": [
-    "js"
-  ],
-  "eslint.packageManager": "yarn",
-  "auto-rename-tag.activationOnLanguage": [
-    "js",
-    "javascript",
-    "*"
-  ],
+
+  "terminal.integrated.fontSize": 16,
+
+  "workbench.startupEditor": "newUntitledFile",
+  "workbench.iconTheme": "material-icon-theme",
+  "workbench.sideBar.location": "left",
+  "workbench.colorTheme": "Night Owl",
+  "workbench.editor.highlightModifiedTabs": true,
+  "workbench.editor.tabSizing": "shrink",
+  "workbench.editor.showTabs": false,
+  "workbench.editor.enablePreviewFromQuickOpen": false,
+
+  "window.zoomLevel": 0,
+
   "extensions.ignoreRecommendations": true,
-  "[mustache]": {}
+
+  "auto-rename-tag.activationOnLanguage": ["js", "javascript", "*"],
+
+  "prettier.disableLanguages": ["js"],
+
+  "[mustache]": {},
+
+  // use emmet for additional syntax
+  "emmet.triggerExpansionOnTab": true,
+  "emmet.includeLanguages": {
+    "javascript": "javascriptreact",
+    "mustache": "html"
+  }
 }
